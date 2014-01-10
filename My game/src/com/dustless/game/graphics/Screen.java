d6751d5d791c1f0f1e19903dce7171d410732f13
Jake <Jake@JakesComputer>
2014-01-10 13:59:25 -0800

    mn,hj,hj,

---
+++ My game/src/com/dustless/game/graphics/Screen.java
@@ -0,0 +1,39 @@
+package com.dustless.game.graphics;
+
+import java.io.InputStream;
+import java.util.Random;
+
+public class Screen {
+	
+	private int width, height;
+	public int[] pixels;
+	
+	private Random random = new Random();
+	
+    public Screen(int width, int height) {
+    	this.width = width;
+    	this.height = height;
+    	pixels = new int[width * height];
+    	
+
+    	}
+
+    public void clear() {
+    	for (int i = 0; i < pixels.length; i++) {
+    		pixels[i] = 0;
+    	}
+    }
+    
+    public void render() {
+    	for (int y = 0; y < height; y++) {
+    		if(y < 0 || y >= height) break;
+    		for (int x = 0; x < width; x++) {
+    			if (x < 0 || x >= width) break;
+    			pixels[x + y * width] = 0xff00ff;
+     		}
+    	}
+    }
+    }
+    
+    	
+
